934644fc32b98d7f4b10474d4a522b59d875c62e
Paula85Garcia <michellgarciagarcia85@gmail.com>
2025-05-26 22:19:56 -0500

    Ejercicio3-el clima de medellin e interfaz

--- ejercicio3/ejercicio3.js
+++ ejercicio3/ejercicio3.js
@@ -1,5 +1,4 @@
 // Ejercicio 3: Obtener el clima actual en Medellín desde tres APIs diferentes
-import fetch from 'node-fetch';
 
 // URLs de las APIs
 const urlopenWttrIn = ('https://wttr.in/Medellin?format=j1')
@@ -52,9 +51,28 @@ async function obtenerClima() {
     // Ejecutar Promise.race para obtener el clima de la primera API que responda
     const resultado = await Promise.race([fetchOpenMeteo, fetchMetNo, fetchWttrIn]);
     console.log(` Clima actual en Medellín desde ${resultado.fuente}: ${resultado.temperatura}${resultado.unidad}`);
+  
+   if (resultado.error) {
+            // Si hay un error, muestra un mensaje en el DOM
+            document.getElementById('weather-info').textContent = 'Error al obtener el clima desde alguna API.';
+            document.getElementById('error-container').classList.remove('hidden');
+        } else {
+            // muestra el clima en el DOM
+            const weatherInfo = document.getElementById('weather-info');
+            weatherInfo.textContent = `Clima actual en Medellín desde ${resultado.fuente}: ${resultado.temperatura}${resultado.unidad}`;
+
+            // Oculta el contenedor de error si no hay errores
+            document.getElementById('error-container').classList.add('hidden');
+        }
+
   } catch (error) {
     console.error(' Error al obtener el clima:', error.message);
+  // Mostrar el error en el DOM
+        const errorMessage = document.getElementById('error-message');
+       document.getElementById('weather-info').textContent = 'Error al obtener el clima';
+        document.getElementById('error-container').classList.remove('hidden');
+    }
+  
   }
-}
 // Llamar a la función para obtener el clima
-obtenerClima();
+window.onload = obtenerClima;
